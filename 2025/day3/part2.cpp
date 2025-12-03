8092f8cd14e2c5835a01899bee687a8e5e0030c8
albanog00 <albanog00@gmail.com>
2025-12-03 21:20:23 +0100

    day3: use only an array

--- 2025/day3/part2.cpp
+++ 2025/day3/part2.cpp
@@ -9,25 +9,20 @@ int main(int argc, char **argv) {
   std::string line;
 
   while (std::cin >> line) {
-    size_t size = line.size();
-    size_t sequence[12];
-
+    char jolt[12];
     size_t next = 0;
-    for (size_t digit = 0; digit <= 11; ++digit) {
-      sequence[digit] = next;
 
-      for (size_t i = sequence[digit] + 1; i < size - (11 - digit); ++i) {
-        if (line[i] > line[sequence[digit]]) {
-          sequence[digit] = i;
+    for (size_t digit = 0; digit <= 11; ++digit) {
+      size_t max = next;
+
+      for (size_t i = max + 1; i < line.size() - (11 - digit); ++i) {
+        if (line[i] > line[max]) {
+          max = i;
         }
       }
 
-      next = sequence[digit] + 1;
-    }
-
-    char jolt[12];
-    for (size_t i = 0; i <= 11; ++i) {
-      jolt[i] = line[sequence[i]];
+      jolt[digit] = line[max];
+      next = max + 1;
     }
 
     count += std::strtoull(jolt, nullptr, 10);
